ac3220250cecf17cdecae915fa2d47e9dbaeb123
unknown <gregxsunday@gmail.com>
2019-05-30 08:17:39 +0200

    added tests

--- lab6/przygotowawcze/aplikacja2_2/app/models/student.rb
+++ lab6/przygotowawcze/aplikacja2_2/app/models/student.rb
@@ -4,4 +4,5 @@ class Student < ApplicationRecord
     validates :surname, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z]+\z/,
         message: "only allows letters" }
     validates :rok, inclusion: { in: [1,2,3,4,5], message: "has to be in range 1-5"}
+    validates :can_edit, inclusion: { in: [true, false]}
 end
